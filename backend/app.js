b0d74292c77b2cffc3dbea510e8988a41f1e3b05
Dan Holodak <danholodak@gmail.com>
2023-04-16 22:19:22 -0400

    user login backend

--- backend/app.js
+++ backend/app.js
@@ -8,17 +8,20 @@ const cors = require('cors');
 const { isProduction } = require('./config/keys');
 
 require('./models/User');
-
+require('./config/passport');
+const passport = require('passport'); 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
 const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
 
 if (!isProduction){
     app.use(cors());
