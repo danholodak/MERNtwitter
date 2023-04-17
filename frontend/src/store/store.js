e225ac697a4a4aa68efe999e7f3c20a3ce0fafdd
Dan Holodak <danholodak@gmail.com>
2023-04-17 08:23:06 -0400

    frontend

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import session from './session';
+import tweets from './tweets'
 
 const rootReducer = combineReducers({
-  
+  session, tweets
 });
 
 let enhancer;
